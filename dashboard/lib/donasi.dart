98e47dcdd9a820408556c7b5a7c7c1fde63d927b
michael <michaelbusiness06@gmail.com>
2022-07-15 15:19:35 +0700

    tambah button donasi dan card menggunakan list view builder

--- dashboard/lib/donasi.dart
+++ dashboard/lib/donasi.dart
@@ -20,7 +20,6 @@ class _DonasiState extends State<Donasi> {
           Expanded(
             child: Container(
               width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.all(15.0),
               child: Text(
                 "Donasi",
                 style: TextStyle(
@@ -37,16 +36,41 @@ class _DonasiState extends State<Donasi> {
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(),
+                    child: ButtonDonasi(
+                      text: "Detail Donasi Aktif",
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: ButtonDonasi(
+                      text: "Riwayat Aksi Donasi",
+                    ),
                   ),
                   Expanded(
                     child: Container(),
                   ),
                   Expanded(
-                    child: Container(),
-                  ),
-                  Expanded(
-                    child: Container(),
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        border: Border.all(color: Colors.black, width: 1),
+                        color: Colors.yellow,
+                      ),
+                      child: Wrap(
+                        direction: Axis.horizontal,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.add),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text('tambah donasi'),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
@@ -61,10 +85,69 @@ class _DonasiState extends State<Donasi> {
                 color: Color.fromARGB(48, 249, 172, 39),
                 borderRadius: BorderRadius.all(Radius.circular(15.0)),
               ),
-              child: Text("Donasi"),
+              child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        print("mencet donasi $index");
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0))),
+                        margin: EdgeInsets.only(bottom: 10),
+                        height: 80.0,
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: [
+                            Text("Donasi $index"),
+                            Spacer(),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15.0))),
+                              child: Text(">"),
+                              padding: EdgeInsets.all(10),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
             ),
           ),
         ]),
+      ),
+    );
+  }
+}
+
+class ButtonDonasi extends StatelessWidget {
+  final String text;
+
+  ButtonDonasi({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (() {
+        print("mencet tombol $text");
+      }),
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          border: Border.all(color: Colors.black, width: 1),
+          color: Colors.yellow,
+        ),
+        child: Text(
+          "$text",
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
